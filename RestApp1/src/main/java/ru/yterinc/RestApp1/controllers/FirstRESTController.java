53af32c7e63b10a958e8025f393329cb4584225b
y.terinc <y.terinc@gmail.com>
2024-09-19 13:06:12 +0700

    RestApp1 - add new controller, model, service, repositories

--- RestApp1/src/main/java/ru/yterinc/RestApp1/controllers/FirstRESTController.java
+++ RestApp1/src/main/java/ru/yterinc/RestApp1/controllers/FirstRESTController.java
@@ -1,15 +1,14 @@
 package ru.yterinc.RestApp1.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController // тоже самое, что @Controller +  @ResponseBody на каждым методом
 @RequestMapping("/api")
 public class FirstRESTController {
 
-    @ResponseBody // без этой анотации Spring искал такой шаблон в каталоге
+    //    @ResponseBody // без этой анотации Spring искал такой шаблон в каталоге
     @GetMapping("/sayHello")
     public String sayHello() {
         return "Hello World";
